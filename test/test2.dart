364cbe71032c261ec2ee4d0720c9a016d162a062
Thyrak12 <thyrak9167@gmail.com>
2025-11-07 10:16:36 +0700

    edit test2

--- test/test2.dart
+++ test/test2.dart
@@ -1,6 +1,5 @@
 import 'package:test/test.dart';
 import '../lib/domain/new.dart';
-import 'dart:convert';
 
 void main() {
   late Department dep;
@@ -11,15 +10,12 @@ void main() {
   late Patient patient;
 
   setUp(() {
+    // Fresh initialization before each test
     dep = Department("Cardiology");
     room1 = Room("101", RoomType.WARD);
     room2 = Room("102", RoomType.ACU);
     bed1 = Bed(bedNumber: "B1");
     bed2 = Bed(bedNumber: "B2");
-    room1.addBed(bed1);
-    room2.addBed(bed2);
-    dep.addRoom(room1);
-    dep.addRoom(room2);
     patient = Patient("Bob", 30);
   });
 
@@ -38,8 +34,7 @@ void main() {
 
     d.addRoom(r1);
     d.addRoom(r2);
-
-    d.removeRoom(r1); // remove by object (you compare id inside)
+    d.removeRoom(r1);
 
     expect(d.rooms.length, 1);
     expect(d.rooms.first.roomNO, "302");
@@ -55,6 +50,7 @@ void main() {
     room1.addBed(bed1);
     room1.addBed(bed2);
     room1.removeBed(bed1.id);
+
     expect(room1.beds.contains(bed1), false);
     expect(room1.beds.contains(bed2), true);
     expect(room1.allBeds().length, 1);
@@ -64,7 +60,7 @@ void main() {
     room1.addBed(bed1);
     room1.addBed(bed2);
 
-    // Mark bed2 as occupied
+    // Mark one bed as occupied
     bed2.isFree = false;
 
     var freeBeds = room1.availableBeds();
@@ -76,6 +72,7 @@ void main() {
   test('All beds in room', () {
     room1.addBed(bed1);
     room1.addBed(bed2);
+
     var allBeds = room1.allBeds();
     expect(allBeds.length, 2);
     expect(allBeds.contains(bed1), true);
@@ -84,41 +81,28 @@ void main() {
 
   test('Assign and free bed via patient', () {
     room1.addBed(bed1);
-    var patient = Patient("Alice", 25);
+    var p = Patient("Alice", 25);
 
     // Assign bed
-    patient.assignBed(bed1);
+    p.assignBed(bed1);
     expect(bed1.isFree, false);
-    expect(patient.currentBedId, bed1.id);
+    expect(p.currentBedId, bed1.id);
 
     // Discharge patient
-    patient.discharge(bed1);
+    p.discharge(bed1);
     expect(bed1.isFree, true);
-    expect(patient.currentBedId, null);
+    expect(p.currentBedId, null);
   });
 
   test('Transfer patient between beds', () {
-    // Assign initial bed
+
     patient.assignBed(bed1);
     expect(bed1.isFree, false);
     expect(patient.currentBedId, bed1.id);
 
-    // Transfer to new bed
     patient.transferBed(bed2, bed1);
     expect(bed1.isFree, true);
     expect(bed2.isFree, false);
     expect(patient.currentBedId, bed2.id);
   });
-
-  test('Multiple rooms in a department', () {
-    expect(dep.rooms.length, 2);
-    expect(dep.rooms.contains(room1), true);
-    expect(dep.rooms.contains(room2), true);
-
-    // Remove a room
-    dep.removeRoom(room1);
-    expect(dep.rooms.contains(room1), false);
-    expect(dep.rooms.length, 1);
-  });
-
 }
